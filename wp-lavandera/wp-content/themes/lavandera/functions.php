cbfcf824d077fa80499044b4fc0f3d40d58a623b
florsepulvedadv <florencia.sepulveda@davinci.edu.ar>
2019-03-08 17:02:35 -0300

    avance home - html

---
+++ wp-lavandera/wp-content/themes/lavandera/functions.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * lavandera functions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage lavandera
+ * @since 1.0
+ * @version 1.0
+ */
+
+
+/*
+ *  Styles
+ */
+function lavandera_styles() {
+	wp_enqueue_style( 'lavandera-fontawesome', get_theme_file_uri( '/assets/css/fontawesome/fontawesome-all.min.css' ) );
+	wp_enqueue_style( 'lavandera-bootstrap', get_theme_file_uri( '/assets/css/vendor/bootstrap/bootstrap.min.css' ) );
+	wp_enqueue_style( 'lavandera-style', get_theme_file_uri( '/assets/css/style.css' ) );
+}
+add_action( 'wp_enqueue_scripts', 'lavandera_styles' );
+
+/*
+ *  Javascript
+ */
+function lavandera_scripts() {
+	wp_enqueue_script( 'lavandera-jquery', get_theme_file_uri( '/assets/js/vendor/jquery/jquery.min.js' ));
+	wp_enqueue_script( 'lavandera-bootstrap', get_theme_file_uri( '/assets/js/vendor/popper/popper.js' ));
+	wp_enqueue_script( 'lavandera-bootstrap', get_theme_file_uri( '/assets/js/vendor/bootstrap/bootstrap.min.js' ));
+	wp_enqueue_script( 'lavandera-js', get_theme_file_uri( '/assets/js/script.js' ));
+}
+add_action( 'wp_enqueue_scripts', 'lavandera_scripts' );
+
--- wp-lavandera/wp-content/themes/lavandera/header.php
+++ wp-lavandera/wp-content/themes/lavandera/header.php
@@ -1,14 +1,90 @@
-<!DOCTYPE html>
-<html lang="es">
+<?php
+/**
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage lavandera
+ * @since 1.0
+ * @version 1.0
+ */
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
 	<head>
-		<title>Horacio Lavandera</title>
-		<meta charset="utf-8" />
+		<title><?php bloginfo('name'); ?> &raquo; <?php is_front_page() ? bloginfo('description') : wp_title(''); ?></title>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" type="text/css" href="css/vendor/bootstrap/bootstrap.min.css">
-		<link href="css/vendor/fontawesome/css/fontawesome-all.min.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
-  
-	<?php wp_head(); ?>
+		<link rel="profile" href="https://gmpg.org/xfn/11" />
+		<?php wp_head(); ?>
 	</head>
-	<body>
+
+	<body <?php body_class(); ?>>
+
+		<header>
+			<nav class="navbar navbar-all">
+				<h1>Horacio Lavandera</h1>
+				<a href="#" class="icon bold black d-sm-none">HL</a>
+				<button class="navbar-toggler menu-toggler-closed" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation" id="menu-toggler">
+				<i class="fas fa-bars black burgerx"></i>
+				</button>
+			</nav>
+			<div class="menu-mobile">
+				<div class="collapse" id="navbarToggleExternalContent">
+					<div class="">
+						<ul class="menu-mobile-list">
+							<li>
+								<a href="#">Inicio</a>
+							</li>
+							<li>
+								<a href="#">Biografía</a>
+							</li>
+							<li>
+								<a href="#">Conciertos</a>
+							</li>
+							<li>
+								<a href="#">Noticias</a>
+							</li>
+							<li>
+								<a href="#">Discrografía</a>
+							</li>
+							<li>
+								<a href="#">Media</a>
+							</li>
+							<li>
+								<a href="#">Contacto</a>
+							</li>
+							<li>
+								<a href="#">Esp</a>
+							</li>
+							<li>
+								<ul class="social-icons">
+									<li>
+										<a href="#">
+											<i class="fab fa-facebook-f blue"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fab fa-twitter blue"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fab fa-instagram blue"></i>
+										</a>
+									</li>
+									<li>
+										<a href="#">
+											<i class="fab fa-youtube blue"></i>
+										</a>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
