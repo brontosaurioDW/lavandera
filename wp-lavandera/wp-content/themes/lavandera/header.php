3533f5c6f3b4c37bb574b894fdbd70891114006d
Laura Mitono <mlauramitono@gmail.com>
2019-03-08 15:40:42 -0300

    Subo nueva carpeta wp-lavandera

---
+++ wp-lavandera/wp-content/themes/lavandera/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Horacio Lavandera</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" type="text/css" href="css/vendor/bootstrap/bootstrap.min.css">
+		<link href="css/vendor/fontawesome/css/fontawesome-all.min.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
+  
+	<?php wp_head(); ?>
+	</head>
+	<body>
